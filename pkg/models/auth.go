8ed2011a5ba3c941b39ade227dde287b8433b86d
Mikołaj <mikolaj.moozoluk@gmail.com>
2025-11-09 02:36:29 +0100

    models for login endpoint

--- pkg/models/auth.go
+++ pkg/models/auth.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type RegisterRequest struct {
 	Email           string `json:"email"`
 	Password        string `json:"password"`
@@ -14,4 +16,24 @@ type RegisterResponse struct {
 	Timezone        string `json:"timezone"`
 	PeriodAnchorDay int    `json:"period_anchor_day"`
 	HomeCurrency    string `json:"home_currency"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	AccessToken      string    `json:"access_token"`
+	ExpiresIn        int64     `json:"expires_in"`
+	TokenType        string    `json:"token_type"`
+	RefreshToken     string    `json:"refresh_token"`
+	RefreshExpiresAt time.Time `json:"refresh_expires_at"`
+}
+
+type LoginInput struct {
+	Email         string
+	PlainPassword string
+	UserAgent     string
+	IP            string
 }
---
+++ pkg/models/sessions.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Session struct {
+	ID               uuid.UUID  `json:"id"`
+	UserID           uuid.UUID  `json:"user_id"`
+	RefreshTokenHash string     `json:"-"`
+	CreatedAt        time.Time  `json:"created_at"`
+	ExpiresAt        time.Time  `json:"expires_at"`
+	RevokedAt        *time.Time `json:"revoked_at"`
+	UserAgent        string     `json:"user_agent"`
+	IP               string     `json:"ip"`
+}
