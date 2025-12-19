02cbdf282bf98dde499cf553d6561d234be5f233
Mikołaj <mikolaj.moozoluk@gmail.com>
2025-12-19 16:53:05 +0100

    categories and product schemas

---
+++ app/schemas/category.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+from uuid import UUID
+
+
+class CategoryCreate(BaseModel):
+    name: str
+    color: str | None = None
+    icon: str | None = None
+
+
+class CategoryRead(BaseModel):
+    id: UUID
+    name: str
+    color: str | None = None
+    icon: str | None = None
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
---
+++ app/schemas/product.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, ConfigDict
+from uuid import UUID
+from datetime import datetime
+from ..schemas.category import CategoryRead
+from ..models import ProductImportance
+
+
+class ProductCreate(BaseModel):
+    name: str
+    category_id: UUID
+    importance: ProductImportance
+
+
+class ProductRead(BaseModel):
+    id: UUID
+    name: str
+    importance: ProductImportance
+    created_at: datetime
+    category: CategoryRead
+
+    model_config = ConfigDict(from_attributes=True)
