014da3f05b4fd85e0ab6c3eedbfba32cf553d255
Mikołaj <mikolaj.moozoluk@gmail.com>
2025-12-22 15:54:07 +0100

    aggregations schemas

---
+++ app/schemas/aggregation.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from .transaction import ProductInTransactionRead
+from .category import CategoryRead
+from decimal import Decimal
+from pydantic import BaseModel
+
+
+class ProductWithSumRead(BaseModel):
+    product: ProductInTransactionRead
+    product_sum: Decimal
+
+
+class CategoriesWithProductsSummaryRead(BaseModel):
+    category: CategoryRead
+    category_sum: Decimal
+    no_product_sum: Decimal
+    products: list[ProductWithSumRead]
+
+
+class CategoriesProductsSummaryRead(BaseModel):
+    currency: str
+    period_start: datetime
+    period_end: datetime
+    total: Decimal
+    categories: list[CategoriesWithProductsSummaryRead]
